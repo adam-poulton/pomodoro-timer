1b25b2412514572046ec41865123f4d1d55dd761
adam-poulton <adam.poulton5@gmail.com>
2019-12-18 00:39:38 +1100

    Add focus/break modes

--- script.js
+++ script.js
@@ -9,10 +9,13 @@ const timerState = {
   startTime: undefined,
   isRunning: false,
   timeout: undefined,
+  mode: 'pomodoro',
+  breakStartTime: undefined,
 }
 
 const defaults = {
   startTime: 1500,
+  breakStartTime: 300,
 }
 
 const alarm = new Audio('alarmwatch.wav');
@@ -35,6 +38,7 @@ const getState = (ctx, key) => {
 
 const setDefaults = () => {
   setState(timerState, 'startTime', getState(defaults, 'startTime'));
+  setState(timerState, 'breakStartTime', getState(defaults, 'breakStartTime'));
 }
 
 const setTimerText = (seconds) => timerValue.textContent = formatTime(seconds)
@@ -50,6 +54,12 @@ const formatTime = (timeSeconds) => {
   return mins + ':' + secs
 }
 
+const toggleButtons = (active) => {
+  document.querySelectorAll("#timer-selection button").forEach(item => 
+    item.classList.remove('active'));
+  document.querySelector("#" + active).classList.add('active');
+}
+
 /**
  * ---------------------------------------------------
  * app
@@ -57,19 +67,20 @@ const formatTime = (timeSeconds) => {
  */
 
 const resetTimer = () => {
-  const startTime = getState(timerState, 'startTime')
+  const mode = getState(timerState, 'mode');
+  let newTime = (mode == 'pomodoro') ? getState(timerState, 'startTime') : getState(timerState, 'breakStartTime');
   setState(timerState, 'isRunning', false);
-  setState(timerState, 'current', startTime);
-  tickTitle(startTime);
-  setTimerText(startTime);
+  setState(timerState, 'current', newTime);
+  tickTitle(newTime);
+  setTimerText(newTime);
 }
 
 const countdown = () => {
   if(getState(timerState, 'isRunning')){
-    seconds = getState(timerState, 'current') - 1
+    seconds = getState(timerState, 'current')
     setTimerText(seconds);
     tickTitle(seconds);
-    setState(timerState, 'current', seconds);
+    setState(timerState, 'current', seconds - 1);
     if(seconds > 0) {
       setTimeout(countdown, 1000);
     } else if(seconds == 0){
@@ -102,9 +113,14 @@ const increment = () => {
   if (getState(timerState, 'isRunning')){
     setState(timerState, 'isRunning', false);
   }
-  const newStartTime = getState(timerState, 'startTime') + 60
-  if (newStartTime > 0) {
+  const mode = getState(timerState, 'mode');
+  let newStartTime;
+  if (mode == 'pomodoro'){
+    newStartTime = Math.min(3600, getState(timerState, 'startTime') + 60)
     setState(timerState, 'startTime', newStartTime)
+  } else if (mode =='break'){
+    newStartTime = Math.min(3600, getState(timerState, 'breakStartTime') + 60)
+    setState(timerState, 'breakStartTime', newStartTime)
   }
   resetTimer();
 }
@@ -113,9 +129,14 @@ const decrement = () => {
   if (getState(timerState, 'isRunning')){
     setState(timerState, 'isRunning', false);
   }
-  const newStartTime = getState(timerState, 'startTime') - 60
-  if (newStartTime > 0) {
+  const mode = getState(timerState, 'mode');
+  let newStartTime;
+  if (mode == 'pomodoro'){
+    newStartTime = Math.max(60, getState(timerState, 'startTime') - 60)
     setState(timerState, 'startTime', newStartTime)
+  } else if(mode == 'break'){
+    newStartTime = Math.max(60, getState(timerState, 'breakStartTime') - 60)
+    setState(timerState, 'breakStartTime', newStartTime);
   }
   resetTimer();
 }
@@ -128,5 +149,13 @@ const setup = () => {
   document.querySelector('#reset-button').addEventListener('click', resetCountdown);
   document.querySelector('#increment').addEventListener('click', increment);
   document.querySelector('#decrement').addEventListener('click', decrement);
+  document.querySelectorAll('#timer-selection button').forEach(item => {
+    item.addEventListener('click', event => {
+      toggleButtons(event.target.id);
+      setState(timerState, 'mode', event.target.id);
+      resetTimer();
+    })
+  })
+
 }
 
